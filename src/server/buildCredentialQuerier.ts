12c3e8bc4ddeb6455cdc0635d7faae3cf291e01e
Sam Davidoff <sam@mfbtech.com>
2024-11-25 16:53:52 +0000

    add server querier

--- src/server/buildCredentialQuerier.ts
+++ src/server/buildCredentialQuerier.ts
@@ -1,12 +1,52 @@
+import http from "http"
+import open from "open"
+
 export function buildCredentialQuerier(deps: {
   debugger?: (str: string) => void
 }): (url: string, responsePort: number) => Promise<string> {
+  const LOCALHOST = "127.0.0.1"
+
   const debug = deps.debugger ? deps.debugger : () => {}
 
   return async (url, responsePort) => {
-    debug(
-      `Will send query to url "${url}" and listen for response at http://localhost:${responsePort}`
-    )
-    return "Fake response"
+    return new Promise<string>((resolve, reject) => {
+      let response: string = ""
+      const server = http.createServer((req, res) => {
+        const rawData: Buffer[] = []
+
+        req.on("open", () => {
+          debug(`Received: "${req.url}"`)
+          response = req.url ?? ""
+        })
+        req.on("close", () => {
+          debug("Request closed.")
+        })
+        req.on("error", err => {
+          debug(`Request received error "${err}"`)
+          reject(err)
+        })
+
+        req.on("data", (chunk: Buffer) => {
+          rawData.push(chunk)
+        })
+
+        req.on("end", () => {
+          debug("Request ended")
+
+          debug(`Received body: "${rawData.join("")}"`)
+          res.writeHead(200)
+
+          debug("Closing server on success")
+          server.close()
+          resolve(response)
+        })
+      })
+
+      debug("Starting temporary redirect server...")
+      server.listen(responsePort, LOCALHOST)
+
+      debug("Opening browser for interactive login...")
+      open(url)
+    })
   }
 }
