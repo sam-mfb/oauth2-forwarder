48b29b11356c15b658bea0a625b27b867bdf8d20
Sam Davidoff <sam@mfbtech.com>
2024-11-25 00:40:57 +0000

    add server logic basics

--- src/server/buildCredentialReceiver.ts
+++ src/server/buildCredentialReceiver.ts
@@ -1,30 +1,11 @@
-import fs from "fs"
 import http from "http"
-import { CredentialOperationHandler, ServerType } from "../types"
-import { gitCredentialIoApi } from "../gitcredential-io"
-import { isCredentialRequestBody } from "../types.guard"
-import { promisify } from "util"
 
-const unlinkAsync = promisify(fs.unlink)
-
-type DepsBase = {
-  type: ServerType
-  credentialOperationHandler: CredentialOperationHandler
+export type Deps = {
+  host: string
+  port: number
+  credentialOperationHandler: () => {}
   debugger?: (str: string) => void
 }
-
-export type Deps = DepsBase &
-  (
-    | {
-        type: "ipc"
-        socketPath: string
-      }
-    | {
-        type: "tcp"
-        host: string
-        port: number
-      }
-  )
 
 export function buildCredentialReceiver(deps: Deps): () => Promise<void> {
   return async () => {
@@ -47,55 +28,45 @@ export function buildCredentialReceiver(deps: Deps): () => Promise<void> {
         debug("Request ended")
         const deserializedBody = JSON.parse(rawData.join(""))
         debug(`Received body: "${rawData.join("")}"`)
-        if (!isCredentialRequestBody(deserializedBody)) {
-          throw new Error(`Body is not in expected format: ${deserializedBody}`)
-        }
-        const credentialRequestBody = deserializedBody
-        debug("Running credential operation handler...")
-        deps
-          .credentialOperationHandler(
-            credentialRequestBody.operation,
-            credentialRequestBody.input
-          )
-          .then(
-            output => {
-              debug("Credential operation handler completed")
-              debug("Sending success header")
-              res.writeHead(200)
-              debug(`Ending response with output: "${JSON.stringify(output)}"`)
-              res.end(gitCredentialIoApi.serialize(output))
-            },
-            reason => {
-              debug(
-                `Credential operation handler errored: "${JSON.stringify(
-                  reason
-                )}"`
-              )
-              debug("Sending error header")
-              res.writeHead(500, JSON.stringify(reason))
-              debug("Ending response")
-              res.end()
-            }
-          )
+
+        debug("Sending success header")
+        res.writeHead(200)
+        debug(`Sending serialized "${deserializedBody}"`)
+        res.end(`Thank you for sending: "${JSON.stringify(deserializedBody)}"`)
+
+        // if (!isCredentialRequestBody(deserializedBody)) {
+        //   throw new Error(`Body is not in expected format: ${deserializedBody}`)
+        // }
+        // const credentialRequestBody = deserializedBody
+        // debug("Running credential operation handler...")
+        // deps
+        //   .credentialOperationHandler(
+        //     credentialRequestBody.operation,
+        //     credentialRequestBody.input
+        //   )
+        //   .then(
+        //     output => {
+        //       debug("Credential operation handler completed")
+        //       debug("Sending success header")
+        //       res.writeHead(200)
+        //       debug(`Ending response with output: "${JSON.stringify(output)}"`)
+        //       res.end(gitCredentialIoApi.serialize(output))
+        //     },
+        //     reason => {
+        //       debug(
+        //         `Credential operation handler errored: "${JSON.stringify(
+        //           reason
+        //         )}"`
+        //       )
+        //       debug("Sending error header")
+        //       res.writeHead(500, JSON.stringify(reason))
+        //       debug("Ending response")
+        //       res.end()
+        //     }
+        //   )
       })
     })
 
-    switch (deps.type) {
-      case "ipc":
-        try {
-          await unlinkAsync(deps.socketPath)
-        } catch (error) {
-          if ((error as NodeJS.ErrnoException).code !== "ENOENT") {
-            throw new Error(`Error removing old socket: ${error}`)
-          }
-        }
-        server.listen(deps.socketPath)
-        break
-      case "tcp":
-        server.listen(deps.port, deps.host)
-        break
-      default:
-        deps satisfies never
-    }
+    server.listen(deps.port, deps.host)
   }
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,5 @@
 import { EnvKey } from "../env"
 import { buildOutputWriter } from "../output"
-import { ServerType } from "../types"
 import { buildCredentialQuerier } from "./buildCredentialQuerier"
 import { type Deps, buildCredentialReceiver } from "./buildCredentialReceiver"
 import { findAvailablePort } from "./findAvailablePort"
@@ -8,7 +7,6 @@ import { findAvailablePort } from "./findAvailablePort"
 const DEBUG = process.env[EnvKey.DEBUG]
 const LOCALHOST = "127.0.0.1"
 const DOCKER_HOST_IP = "host.docker.internal"
-const GIT_CMD = "git"
 
 const appOutput = buildOutputWriter({ color: "cyan", stream: process.stdout })
 const instructions = buildOutputWriter({
@@ -21,17 +19,9 @@ const configOutput = buildOutputWriter({
 })
 const errorOutput = buildOutputWriter({ color: "red", stream: process.stderr })
 
-let serverType: ServerType = "tcp"
-let socket = ""
-const socketEnv = process.env[EnvKey.SOCKET]
-if (socketEnv) {
-  serverType = "ipc"
-  socket = socketEnv
-}
-
 let userSpecifiedPort: number | null = null
 const portEnv = process.env[EnvKey.PORT]
-if (serverType === "tcp" && portEnv) {
+if (portEnv) {
   const parsedPort = parseInt(portEnv)
   if (!isNaN(parsedPort)) {
     userSpecifiedPort = parsedPort
@@ -39,18 +29,7 @@ if (serverType === "tcp" && portEnv) {
 }
 
 ;(async () => {
-  let gitPath = process.env[EnvKey.GIT_PATH]
-  if (gitPath) {
-    errorOutput(
-      `App is using git command at path specified by environmental variable ${EnvKey.GIT_PATH}=${gitPath}. Make sure this is what you intended.`
-    )
-  } else {
-    gitPath = GIT_CMD
-  }
-
   const credentialQuerier = buildCredentialQuerier({
-    externalEnv: process.env,
-    gitPath: gitPath,
     debugger: DEBUG
       ? buildOutputWriter({ color: "magenta", stream: process.stdout })
       : undefined
@@ -65,52 +44,23 @@ if (serverType === "tcp" && portEnv) {
 
   let deps: Deps
 
-  switch (serverType) {
-    case "ipc":
-      deps = {
-        ...baseDeps,
-        type: "ipc",
-        socketPath: socket
-      }
-      break
-    case "tcp": {
-      if (userSpecifiedPort) {
-        appOutput(`Attempting to use user specified port ${userSpecifiedPort}`)
-      }
-      const port = userSpecifiedPort ?? (await findAvailablePort(LOCALHOST))
-      deps = {
-        ...baseDeps,
-        type: "tcp",
-        host: LOCALHOST,
-        port
-      }
-      break
-    }
+  if (userSpecifiedPort) {
+    appOutput(`Attempting to use user specified port ${userSpecifiedPort}`)
+  }
+  const port = userSpecifiedPort ?? (await findAvailablePort(LOCALHOST))
+  deps = {
+    ...baseDeps,
+    host: LOCALHOST,
+    port
   }
   const credentialReceiver = buildCredentialReceiver(deps)
-  switch (deps.type) {
-    case "ipc":
-      appOutput(`Starting IPC server listening on socket ${deps.socketPath}`)
-      instructions(
-        `Bind mount this socket into your docker container and run the following command on your docker container (assuming you bind the socket at the same path):\n`
-      )
-      instructions(`    export ${EnvKey.SERVER}="${deps.socketPath}"\n`)
-      break
-    case "tcp":
-      appOutput(`Starting TCP server listening on ${deps.host}:${deps.port}`)
-      instructions(`Run the following command in your docker container:\n`)
-      configOutput(
-        `    export ${EnvKey.SERVER}="${
-          deps.host === LOCALHOST ? DOCKER_HOST_IP : deps.host
-        }:${deps.port}"\n`
-      )
-      break
-  }
-  instructions(
-    `Edit your git configuration file inside your docker container to call the git-credential-forwarder client script, for example:\n`
+  appOutput(`Starting TCP server listening on ${deps.host}:${deps.port}`)
+  instructions(`Run the following command in your docker container:\n`)
+  configOutput(
+    `    export ${EnvKey.SERVER}="${
+      deps.host === LOCALHOST ? DOCKER_HOST_IP : deps.host
+    }:${deps.port}"\n`
   )
-  configOutput(`   [credential]`)
-  configOutput(`     helper = "!f() { node ~/gcf-client.js $*; }; f"\n`)
 
   try {
     await credentialReceiver()
