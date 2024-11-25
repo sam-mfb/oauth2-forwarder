7f600f29cab911c7442daaea151f4842768ce771
Sam Davidoff <sam@mfbtech.com>
2024-11-25 23:55:10 +0000

    add http redirect logic

--- src/client/buildRedirect.ts
+++ src/client/buildRedirect.ts
@@ -1,9 +1,19 @@
+import http from "http"
+
 export function buildRedirect(deps: {
   debugger?: (str: string) => void
 }): (url: string) => Promise<void> {
   const debug = deps.debugger ? deps.debugger : () => {}
   return async url => {
     debug(`Making GET request to url: "${url}"`)
-    return
+    return new Promise((resolve, reject) => {
+      http.get(url, res => {
+        if (res.statusCode !== 200) {
+          reject(`Request returned unexpected status: ${res.statusCode}`)
+        } else {
+          resolve()
+        }
+      })
+    })
   }
 }
