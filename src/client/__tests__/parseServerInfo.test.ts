6bb557d83dfbdb89d6ec85ba075e8ad3926ea01e
Sam Davidoff <sam@mfbtech.com>
2024-11-26 20:16:18 +0000

    fix parse server test

--- src/client/__tests__/parseServerInfo.test.ts
+++ src/client/__tests__/parseServerInfo.test.ts
@@ -9,10 +9,6 @@ describe(parseServerInfo.name, () => {
     if (Result.isFailure(result)) {
       throw new Error()
     }
-    expect(result.value.type).toEqual("tcp")
-    if (!(result.value.type === "tcp")) {
-      throw new Error()
-    }
     expect(result.value.host).toEqual("example.com")
     expect(result.value.port).toEqual(3000)
   })
@@ -37,33 +33,6 @@ describe(parseServerInfo.name, () => {
 
   it("returns failure when host is missing", () => {
     const info = ":3000"
-    const result = parseServerInfo(info)
-    expect(Result.isFailure(result)).toBeTruthy()
-  })
-
-  it("returns correct ipc server information for valid socket path", () => {
-    const info = "/tmp/socket"
-    jest.spyOn(fs, "existsSync").mockReturnValue(true)
-    const result = parseServerInfo(info)
-    if (Result.isFailure(result)) {
-      throw new Error()
-    }
-    expect(result.value.type).toEqual("ipc")
-    if (!(result.value.type === "ipc")) {
-      throw new Error()
-    }
-    expect(result.value.socketPath).toEqual("/tmp/socket")
-  })
-
-  it("returns failure when socket path does not exist", () => {
-    const info = "/invalid/path"
-    jest.spyOn(fs, "existsSync").mockReturnValue(false)
-    const result = parseServerInfo(info)
-    expect(Result.isFailure(result)).toBeTruthy()
-  })
-
-  it("returns failure for incorrect format", () => {
-    const info = "invalid_input"
     const result = parseServerInfo(info)
     expect(Result.isFailure(result)).toBeTruthy()
   })
